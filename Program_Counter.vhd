b2310cc6f852a925d7daa63e1010f861516a2905
Hunter Goodson <hjgoodson42@students.tntech.edu>
2018-03-11 20:38:34 -0500

    Updated Program counter to 32 bit

--- Program_Counter.vhd
+++ Program_Counter.vhd
@@ -2,10 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Program_Counter is --Glorified 64-bit register
-	port(D: in std_logic_vector (63 downto 0);
+entity Program_Counter is --Glorified 32-bit register
+	port(D: in std_logic_vector (31 downto 0);
 		 CLR,CLK: in std_logic;
-		 Q: out std_logic_vector (63 downto 0));
+		 Q: out std_logic_vector (31 downto 0));
 end Program_Counter;
 
 
