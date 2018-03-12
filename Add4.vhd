eb0800ca1f0a8c5c5b0ee86a46c8d2b86a92dac6
Hunter Goodson <hjgoodson42@students.tntech.edu>
2018-03-11 20:30:37 -0500

    Adder and Coutner

---
+++ Add4.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.std_logic_arith.all;
+use ieee.math_real.all;
+use ieee.numeric_std.all;
+
+entity Add4 is
+	port(A: in std_logic_vector (31 downto 0);
+		 --B: in std_logic_vector (2 downto 0);
+		 Q: out std_logic_vector (31 downto 0));
+end Add4;
+
+architecture a of Add4 is
+constant four : std_logic_vector (2 downto 0) := "100"; 
+begin
+	process(A)
+	begin
+		Q <= std_logic_vector(to_unsigned(to_integer(unsigned(A)) + four,64));
+	end process;
+end a;
---
+++ Program_Counter.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Program_Counter is --Glorified 64-bit register
+	port(D: in std_logic_vector (63 downto 0);
+		 CLR,CLK: in std_logic;
+		 Q: out std_logic_vector (63 downto 0));
+end Program_Counter;
+
+
+
+architecture pc of Program_Counter is
+begin
+	process (CLK,CLR)
+	begin
+		if(CLR='1') then
+			Q <= x"0000000000000000";
+		elsif rising_edge(clk) then
+				Q <= D;
+		end if;
+	end process;
+end pc;
