43d9e82a47179aad4729e5c044ca2fb555986e55
CameronJay <26509055+CameronJay@users.noreply.github.com>
2018-03-14 12:14:56 -0500

    Add files via upload

--- Phase_1.vhd
+++ Phase_1.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity Phase_1 is
   port (  Clear, Clock :in STD_LOGIC;
-			 Iout : out STD_LOGIC_VECTOR(31 DOWNTO 0));
+			 Iout, Pout : out STD_LOGIC_VECTOR(31 DOWNTO 0));
 end Phase_1;
 
 architecture structure of Phase_1 is
@@ -37,11 +37,12 @@ signal IMin: STD_LOGIC_VECTOR (7 DOWNTO 0);
 
 begin --Port mapping for each device
   U1: Add4 port map (PCout, ADDout);
-  U2: Program_Counter port map (ADDout, Clear, Clock, PCout);
+  U2: Program_Counter port map (D => ADDout, CLR => Clear, CLK => Clock, Q => PCout);
   U3: IP_Catalog port map (address => IMin, clock => Clock, wren => '0', data => "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", q => Iout); --Data doesn't matter yet
   
   process(Clock, PCout)
   begin
 	IMin <= PCout(7 DOWNTO 0);
+	Pout <= PCout(31 DOWNTO 0);
   end process; 
 end structure;
--- tb_Phase_1.vhd
+++ tb_Phase_1.vhd
@@ -8,12 +8,15 @@ architecture tb_Phase_1_arch of tb_Phase_1 is
 	signal Clear: STD_LOGIC := '0';
 	signal Clock : STD_LOGIC := '0';
 	signal Iout : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	signal Pout : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	
 component Phase_1 IS
   port (  Clear, Clock :in STD_LOGIC;
-			 Iout : out STD_LOGIC_VECTOR(31 DOWNTO 0));
+			 Iout, Pout : out STD_LOGIC_VECTOR(31 DOWNTO 0));
 	END component ;
+
 	begin
-		DUT: Phase_1 port map (Clear,Clock, Iout(31 DOWNTO 0));
+		DUT: Phase_1 port map (Clear,Clock, Iout(31 DOWNTO 0), Pout(31 DOWNTO 0));
 			Clock <= not Clock after 20ns;
 			Clear <= '1' after 100ns,
 						'0' after 300ns;
